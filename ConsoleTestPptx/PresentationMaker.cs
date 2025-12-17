72c1013f47d5aef88d7322c835a90f3a4e390b7c
anatoly <anatoly.belanov.0798@gmail.com>
2025-12-17 10:33:53 +0300

    second title

--- ConsoleTestPptx/PresentationMaker.cs
+++ ConsoleTestPptx/PresentationMaker.cs
@@ -14,12 +14,12 @@ public class PresentationMaker
 
 	public async Task BuildPresentation()
 	{
-		AddTitleToFirstSlide();
+		FormatFirstSlide();
 		var filename = await CreateFilename();
 		await SavePresentation(filename);
 	}
 
-	private void AddTitleToFirstSlide()
+	private void FormatFirstSlide()
 	{
 		var slide = _presentationScope.Presentation.Slides.Count > 0
 			? _presentationScope.Presentation.Slides[0]
@@ -40,6 +40,22 @@ public class PresentationMaker
 		portion.PortionFormat.FontHeight = 48;
 		portion.PortionFormat.FillFormat.FillType = FillType.Solid;
 		portion.PortionFormat.FillFormat.SolidFillColor.Color = System.Drawing.Color.Black;
+
+		var secondTextWidth = 400f;
+		var secondTextHeight = 70f;
+		var secondX = ((float)slideSize.Width - secondTextWidth) / 2;
+		var secondY = y + textHeight + 50f;
+		
+		var secondTextBox = slide.Shapes.AddAutoShape(ShapeType.Rectangle, secondX, secondY, secondTextWidth, secondTextHeight);
+		secondTextBox.FillFormat.FillType = FillType.NoFill;
+		secondTextBox.LineFormat.FillFormat.FillType = FillType.NoFill;
+		secondTextBox.Rotation = -30f;
+		secondTextBox.TextFrame.Text = "Второй текст";
+		secondTextBox.TextFrame.Paragraphs[0].ParagraphFormat.Alignment = TextAlignment.Center;
+		var secondPortion = secondTextBox.TextFrame.Paragraphs[0].Portions[0];
+		secondPortion.PortionFormat.FontHeight = 36;
+		secondPortion.PortionFormat.FillFormat.FillType = FillType.Solid;
+		secondPortion.PortionFormat.FillFormat.SolidFillColor.Color = System.Drawing.Color.Purple;
 	}
 
 	private async Task<string> CreateFilename()
