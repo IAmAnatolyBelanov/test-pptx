4b7e476233b21efb260333ce3e443c1a55ec6ffa
anatoly <anatoly.belanov.0798@gmail.com>
2025-12-17 17:02:21 +0300

    license

--- ConsoleTestPptx/Program.cs
+++ ConsoleTestPptx/Program.cs
@@ -1,4 +1,4 @@
-﻿using Aspose.Slides;
+using Aspose.Slides;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +9,8 @@ public class Program
 {
 	public static async Task Main(string[] args)
 	{
+		LoadLicenseIfExists();
+
 		var builder = Host.CreateApplicationBuilder(args);
 		builder.Services.TryAddScoped<PresentationScope>();
 		builder.Services.TryAddScoped<PresentationMaker>();
@@ -20,5 +22,22 @@ public class Program
 		presentationScope.Presentation = new Presentation();
 		var maker = scope.ServiceProvider.GetRequiredService<PresentationMaker>();
 		await maker.BuildPresentation();
+	}
+
+	private static void LoadLicenseIfExists()
+	{
+		var baseDirectory = AppContext.BaseDirectory;
+		var licenseFiles = new[] { "Aspose.Slides.lic", "license.lic", "Aspose.Slides.lic.xml" };
+
+		foreach (var licenseFile in licenseFiles)
+		{
+			var licensePath = Path.Combine(baseDirectory, licenseFile);
+			if (File.Exists(licensePath))
+			{
+				var license = new License();
+				license.SetLicense(licensePath);
+				return;
+			}
+		}
 	}
 }
